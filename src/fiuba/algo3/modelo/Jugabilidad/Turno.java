02645e02f630a46f61ad6ed2ff91c33ed150cb85
josetec <jose@tecnowall.com.ar>
2016-06-09 09:15:51 -0300

    Se crea la clase turno

---
+++ src/fiuba/algo3/modelo/Jugabilidad/Turno.java
@@ -0,0 +1,46 @@
+package fiuba.algo3.modelo.Jugabilidad;
+
+/**
+ * Created by jose on 09/06/2016.
+ */
+public class Turno {
+
+    private int siguiente;
+    private Jugador[] turnos = new Jugador [2];
+
+
+
+    public Turno (Jugador player1, Jugador player2){
+
+
+        turnos[0] = player1;
+        turnos[1] = player2;
+
+        int turnoAlAzar =  (int) (Math.random()*turnos.length);
+
+        this.siguiente= turnoAlAzar;
+
+
+
+    }
+
+    //este metodo es para test
+public Jugador getTurno (){
+
+    return this.turnos [siguiente];
+
+}
+
+    public Jugador siguiente(){
+
+        int auxiliar = this.siguiente;
+        this.siguiente = this.siguiente +1;
+        if (this.siguiente >= this.turnos.length) this.siguiente=0;
+
+        return this.turnos [auxiliar];
+
+    }
+
+
+
+}
